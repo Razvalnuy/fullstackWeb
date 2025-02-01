d3a9f499112e755670e633704a346d994ffbf8c1
razvalnuy <megamg001@gmail.com>
2025-02-01 17:08:17 +0300

    topBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Container, Title } from "@/components/shared"
+import { TopBar } from "@/components/shared/topbar"
 
 export default function Home() {
 	return (
-		<Container className="mt-10">
-			<Title text="Все пиццы" size="lg" className="font-extrabold" />
-		</Container>
+		<>
+			<Container className="mt-10">
+				<Title text="Все пиццы" size="lg" className="font-extrabold" />
+			</Container>
+			<TopBar />
+		</>
 	)
 }
