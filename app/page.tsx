977efbdf500284bbaa8ebdcda399fe8bc17724a7
razvalnuy <megamg001@gmail.com>
2025-02-07 02:14:52 +0300

    ingredients

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import { Container, Title } from "@/components/shared"
-import { TopBar } from "@/components/shared/topbar"
+import { Container, Filters, Title, TopBar } from "@/components/shared"
 
 export default function Home() {
 	return (
@@ -7,7 +6,22 @@ export default function Home() {
 			<Container className="mt-10">
 				<Title text="Все пиццы" size="lg" className="font-extrabold" />
 			</Container>
+
 			<TopBar />
+
+			<Container className="mt-10 pb-14">
+				<div className="flex gap-[64px]">
+					{/* Фильтрация */}
+					<div className="w-[250px]">
+						<Filters />
+					</div>
+
+					{/* Список товаров */}
+					<div className="flex-1">
+						<div className="flex flex-col gap-16">Список товаров</div>
+					</div>
+				</div>
+			</Container>
 		</>
 	)
 }
--- components/shared/top-bar.tsx
+++ components/shared/top-bar.tsx
@@ -1,8 +1,6 @@
 import { cn } from "@/lib/utils"
 import React from "react"
-import { Categories } from "./categories"
-import { Container } from "./container"
-import { SortPopup } from "./sort-popup"
+import { Categories, Container, SortPopup } from "../shared"
 
 interface Props {
 	className?: string
